42ff86cae3ae7dff2994aa24426a52836c8e0f12
Schmidt Benedek <schmidt.benedek_2022C@bolyia-szakkozep.hu>
2025-12-22 19:45:11 +0100

    változó deklarálás

--- colspanjs/index.js
+++ colspanjs/index.js
@@ -1,6 +1,115 @@
-console.log("Szerző | Mű | Fogalmak | |") //Consolera kiírom a táblázat fejlécét
-console.log("Appolliniare | A megsebzett galamb és a szökőkú | képvers | emlékezés") //Kiírom consolera a táblázat 1. sorát
-console.log("Apollinaire | Búcsú | avantgárd | |") //Kiírom consolera a táblázat 2. sorát
-console.log("Thomas Mann | Mario és a varázsló | kisregény | |") //Kiírom consolera a táblázat 3. sorát
-console.log("Franz Kafka | A per | regény | |") //Kiírom consolera a táblázat 4. sorát
-console.log("Franz Kafka | Az átvlátozás | kisregény | groteszk") //Kiírom consolera a táblázat 5. sorát
+//Fejlec
+
+/**
+ * @type {string} //A Szerzo oszlop fejlece 
+ */
+const headSzerzo = "Szerző" //Valtozo deklaralas a fejlec elso cellajanak
+
+/**
+ * @type {string} // A Mu oszlop fejlece
+ */
+const headMu = "Mű" //Valtzozo deklaralasa a fejlec masodik cellajanak
+
+/**
+ * @type {string} //A Fogalmak oszlop fejlece
+ */
+const headFogalmak = "Fogalmak" //Valtzozo deklaralasa a fejlec harmadik cellajanak
+
+
+//1. sor
+
+/**
+ * @type {string} //Az elso sor elso ertekenek
+ */
+const rowFirstSzerzo = "Apollinaire" //Valtozo deklaralas a tablazat elso soranak elso cellajahoz
+/**
+ * @type {string} //Az elso sor masodik ertekenek
+ */
+const rowFirstMu = "A megsebzett galamb és a szökőkút" //Valtozo deklaralas a tablazat elso soranak masodik cellajahoz
+/**
+ * @type {string} //Az elso sor harmadik ertekenek
+ */
+const rowFirstFogalmakFirst = "képvers" //Valtozo deklaralas a tablazat elso soranak harmadik cellajahoz
+/**
+ * @type {string} //Az elso sor negyedik ertekenek
+ */
+const rowFirstFogalmakSecond = "emlékezés" //Valtozo deklaralas a tablazat elso soranak negyedik cellajahoz
+
+
+//2. sor
+
+/**
+ * @type {string} //A masodik sor elso ertekenek
+ */
+const rowSecondSzerzo = "Apollinaire" //Valtozo deklaralas a tablazat masodik soranak elso cellajahoz
+/**
+ * @type {string} //A masodik sor masodik ertekenek
+ */
+const rowSecondMu = "Búcsú" //Valtozo deklaralas a tablazat masodik soranak masodik cellajahoz
+/**
+ * @type {string} //A masodik sor harmadik ertekenek
+ */
+const rowSecondFogalmakFirst = "avantgárd" //Valtozo deklaralas a tablazat masodik soranak harmadik cellajahoz
+
+
+
+//3. sor
+
+/**
+ * @type {string} //A harmadik sor elso ertekenek
+ */
+const rowThirdSzerzo = "Thomas Mann" //Valtozo deklaralas a tablazat harmadik soranak elso cellajahoz
+/**
+ * @type {string} //A harmadik sor masodik ertekenek
+ */
+const rowThirdMu = "Mario és a varázsló" //Valtozo deklaralas a tablazat harmadik soranak masodik cellajahoz
+/**
+ * @type {string} //A harmadik sor harmadik ertekenek
+ */
+const rowThirdFogalmakFirst = "kisregény" //Valtozo deklaralas a tablazat harmadik soranak harmadik cellajahoz
+
+
+
+//4. sor
+
+/**
+ * @type {string} //A negyedik sor elso ertekenek
+ */
+const rowFourthSzerzo = "Franz Kafka" //Valtozo deklaralas a tablazat negyedik soranak elso cellajahoz
+/**
+ * @type {string} //A negyedik sor masodik ertekenek
+ */
+const rowFourthMu = "A per" //Valtozo deklaralas a tablazat negyedik soranak masodik cellajahoz
+/**
+ * @type {string} //A negyedik sor harmadik ertekenek
+ */
+const rowFourthFogalmakFirst = "regény" //Valtozo deklaralas a tablazat negyedik soranak harmadik cellajahoz
+
+
+
+
+//5. sor
+
+/**
+ * @type {string} //Az otodik sor elso ertekenek
+ */
+const rowFifthSzerzo = "Franz Kafka" //Valtozo deklaralas a tablazat otodik soranak elso cellajahoz
+/**
+ * @type {string} //Az otodik sor masodik ertekenek
+ */
+const rowFifthMu = "Az átváltozás" //Valtozo deklaralas a tablazat otodik soranak masodik cellajahoz
+/**
+ * @type {string} //Az otodik sor harmadik ertekenek
+ */
+const rowFifthFogalmakFirst = "kisregény" //Valtozo deklaralas a tablazat otodik soranak harmadik cellajahoz
+/**
+ * @type {string} //Az otodik sor negyedik ertekenek
+ */
+const rowFifthFogalmakSecond = "groteszk" //Valtozo deklaralas a tablazat otodik soranak negyedik cellajahoz
+
+console.log(headSzerzo + " | " + headMu + " | " + headFogalmak + " | |") //Kiiratom a tablazat fejlecet a consolera
+console.log(rowFirstSzerzo + " | " + rowFirstMu + " | " + rowFirstFogalmakFirst + " | " + rowFirstFogalmakSecond) //Kiiratom a tablazat elso sorat
+console.log(rowSecondSzerzo + " | " + rowSecondMu + " | " + rowSecondFogalmakFirst + " | |") //Kiiratom a tablazat masodik sorat
+console.log(rowThirdSzerzo + " | " + rowThirdMu + " | " + rowThirdFogalmakFirst + " | |") //Kiiratom a tablazat harmadik sorat
+console.log(rowFourthSzerzo + " | " + rowFourthMu + " | " + rowFourthFogalmakFirst + " | |") //Kiiratom a tablazat negyedik sorat
+console.log(rowFifthSzerzo + " | " + rowFifthMu + " | " + rowFifthFogalmakFirst + " | " + rowFifthFogalmakSecond) //Kiiratom a tablazat otodik sorat
