2fbe01a8be8d1db29b86dd13605368f7b840fb22
Schmidt Benedek <schmidt.benedek_2022C@bolyia-szakkozep.hu>
2025-12-24 11:15:36 +0100

    console col elagazas

--- colspanjs/index.js
+++ colspanjs/index.js
@@ -43,15 +43,61 @@ const dataArr = [ //tombot deklaralok
 
 console.log(fejlec[0] + " | " + fejlec[1] + " | " + fejlec[2] + " | |"); //Fejlec consolera iratasa
 
-console.log(dataArr[0].writer + " | " + dataArr[0].work + " | " + dataArr[0].concept1 + " | " + dataArr[0].concept2 + " |"); //1. sor consolera kiiratasa
-console.log(dataArr[1].writer + " | " + dataArr[1].work + " | " + dataArr[1].concept1 + " | |"); //2. sor consolera kiiratasa
-console.log(dataArr[2].writer + " | " + dataArr[2].work + " | " + dataArr[2].concept1 + " | |"); //3. sor consolera kiiratasa
-console.log(dataArr[3].writer + " | " + dataArr[3].work + " | " + dataArr[3].concept1 + " | |"); //4. sor consolera kiiratasa
-console.log(dataArr[4].writer + " | " + dataArr[4].work + " | " + dataArr[4].concept1 + " | " + dataArr[4].concept2 + " |"); //5. sor consolera kiiratasa
+/**
+ * @type {string} Elso sor teljes tartalmanka tarolasa
+ */
+let firstRow = dataArr[0].writer + " | " + dataArr[0].work + " | "; //Alap ertekeket megadom
+if (dataArr[0].concept2 != undefined) {  //Megvizsgalom hogy a concept2 erteke definalva van e
+    firstRow += dataArr[0].concept1 + " | " + dataArr[0].concept2 + " |"; //Es ha definialva van akkor a hianyzo 2 cellat hosszafuzom az alap sorhoz
+} else { //Maskulonben ez fog lefutni
+    firstRow += dataArr[0].concept1 + " | |"; //Es hozzafuzi a maradek hianyzo adatot a sorhoz
+}
+console.log(firstRow); //Kiiratom az 1. sort a consolera
 
 
+/**
+ * @type {string} Masodik sor teljes tartalmanak tarolasa
+ */
+let secondRow = dataArr[1].writer + " | " + dataArr[1].work + " | "; //Alap ertekeket megadom
+if (dataArr[1].concept2 != undefined) { //Megvizsgalom hogy a concept2 erteke definalva van e
+    secondRow += dataArr[1].concept1 + " | " + dataArr[1].concept2 + " |"; //Es ha definialva van akkor a hianyzo 2 cellat hosszafuzom az alap sorhoz
+} else { //Maskulonben ez fog lefutni
+    secondRow += dataArr[1].concept1 + " | |"; //Es hozzafuzi a maradek hianyzo adatot a sorhoz
+}
+console.log(secondRow); //Kiiratom a 2. sort a consolera
 
 
+/**
+ * @type {string} Harmadik sor teljes tartalmanak tarolasa
+ */
+let thirdRow = dataArr[2].writer + " | " + dataArr[2].work + " | "; //Alap ertekeket megadom
+if (dataArr[2].concept2 != undefined) { //Megvizsgalom hogy a concept2 erteke definalva van e
+    thirdRow += dataArr[2].concept1 + " | " + dataArr[2].concept2 + " |"; //Es ha definialva van akkor a hianyzo 2 cellat hosszafuzom az alap sorhoz
+} else { //Maskulonben ez fog lefutni
+    thirdRow += dataArr[2].concept1 + " | |"; //Es hozzafuzi a maradek hianyzo adatot a sorhoz
+}
+console.log(thirdRow); //Kiiratom a 3. sort a consolera
 
 
+/**
+ * @type {string} Negyedik sor teljes tartalmanak tarolasa
+ */
+let fourthRow = dataArr[3].writer + " | " + dataArr[3].work + " | "; //Alap ertekeket megadom
+if (dataArr[3].concept2 != undefined) { //Megvizsgalom hogy a concept2 erteke definalva van e
+    fourthRow += dataArr[3].concept1 + " | " + dataArr[3].concept2 + " |"; //Es ha definialva van akkor a hianyzo 2 cellat hosszafuzom az alap sorhoz
+} else { //Maskulonben ez fog lefutni
+    fourthRow += dataArr[3].concept1 + " | |"; //Es hozzafuzi a maradek hianyzo adatot a sorhoz
+}
+console.log(fourthRow); //Kiiratom a 4. sort a consolera
 
+
+/**
+ * @type {string} Otodik sor teljes tartalmanak tarolasa
+ */
+let fifthRow = dataArr[4].writer + " | " + dataArr[4].work + " | "; //Alap ertekeket megadom
+if (dataArr[4].concept2 != undefined) { //Megvizsgalom hogy a concept2 erteke definalva van e
+    fifthRow += dataArr[4].concept1 + " | " + dataArr[4].concept2 + " |"; //Es ha definialva van akkor a hianyzo 2 cellat hosszafuzom az alap sorhoz
+} else { //Maskulonben ez fog lefutni
+    fifthRow += dataArr[4].concept1 + " | |"; //Es hozzafuzi a maradek hianyzo adatot a sorhoz
+}
+console.log(fifthRow); //Kiiratom az 5. sort a consolera
