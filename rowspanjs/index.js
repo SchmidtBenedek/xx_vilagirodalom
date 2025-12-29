6f1282301a568f0d3b9d1664c2de39218ca4686b
Schmidt Benedek <schmidt.benedek_2022C@bolyia-szakkozep.hu>
2025-12-29 12:16:15 +0100

    console row tablazat

---
+++ rowspanjs/index.js
@@ -0,0 +1,6 @@
+console.log("Szerző | Mű | Fogalmak |") //A consolera kiírom a táblázat fejlécét
+console.log("Appolliniare | A megsebzett galamb és a szökőkú | képvers |") //kiírom a consolera a táblázat 1. sorát
+console.log("Apollinaire | Búcsú | avantgárd |") //kiírom a consolera a táblázat 2. sorát
+console.log("Thomas Mann | Mario és a varázsló | kisregény |") //kiírom a a táblázat 3. sorát
+console.log("Franz Kafka | A per | regény |") //kiírom a consolera a táblázat 4. sorát
+console.log("Franz Kafka | Az átvlátozás | kisregény |") //kiírom a consolera a táblázat 5. sorát
