0736e2ce63017a23bae82134f8979f0e09208e76
Schmidt Benedek <schmidt.benedek_2022C@bolyia-szakkozep.hu>
2025-12-29 12:45:24 +0100

    console row valtozo bev

--- rowspanjs/index.js
+++ rowspanjs/index.js
@@ -1,6 +1,117 @@
-console.log("Szerző | Mű | Fogalmak |") //A consolera kiírom a táblázat fejlécét
-console.log("Appolliniare | A megsebzett galamb és a szökőkú | képvers |") //kiírom a consolera a táblázat 1. sorát
-console.log("Apollinaire | Búcsú | avantgárd |") //kiírom a consolera a táblázat 2. sorát
-console.log("Thomas Mann | Mario és a varázsló | kisregény |") //kiírom a a táblázat 3. sorát
-console.log("Franz Kafka | A per | regény |") //kiírom a consolera a táblázat 4. sorát
-console.log("Franz Kafka | Az átvlátozás | kisregény |") //kiírom a consolera a táblázat 5. sorát
+// Fejlec
+
+/**
+ * @type {string} A Szerző oszlop fejlece
+ */
+const headSzerzo = "Szerző" //Valtozo deklaralas a fejlec elso cellajanak
+
+/** 
+ * @type {string} A Mű oszlop fejlece
+ */
+const headMu = "Mű" //Valtozo deklaralas a fejlec masodik cellajanak
+
+/** 
+ * @type {string} A Fogalmak oszlop fejlece
+ */
+const headFogalmak = "Fogalmak" //Valtozo deklaralas a fejlec harmadik cellajanak
+
+
+//1. Sor
+
+/** 
+ * @type {string} Az elso sor elso ertekenek
+ */
+const rowFirstSzerzo = "Apollinaire" //Az elso sor elso cellajanak valtozoja
+
+/** 
+* @type {string} Az elso sor masodik ertekenek
+*/
+const rowFirstMu = "A megsebzett galamb és a szökőkút" //Az elso sor masodik cellajanak valtozoja
+
+/** 
+* @type {string} Az elso sor harmadik ertekenek
+*/
+const rowFirstFogalmak = "képvers" //Az elso sor harmadik cellaja valtozojanak valtozoja
+
+
+//2. sor
+
+/**
+ * @type {string} //A masodik sor elso ertekenek
+ */
+const rowSecondSzerzo = "Apollinaire" //Valtozo deklaralas a tablazat masodik soranak elso cellajahoz
+
+/**
+ * @type {string} //A masodik sor masodik ertekenek
+ */
+const rowSecondMu = "Búcsú" //Valtozo deklaralas a tablazat masodik soranak masodik cellajahoz
+
+/**
+ * @type {string} //A masodik sor harmadik ertekenek
+ */
+const rowSecondFogalmak = "avantgárd" //Valtozo deklaralas a tablazat masodik soranak harmadik cellajahoz
+
+
+//3. sor
+
+/**
+ * @type {string} //A harmadik sor elso ertekenek
+ */
+
+const rowThirdSzerzo = "Thomas Mann" //Valtozo deklaralas a tablazat harmadik soranak elso cellajahoz
+
+/**
+ * @type {string} //A harmadik sor masodik ertekenek
+ */
+
+const rowThirdMu = "Mario és a varázsló" //Valtozo deklaralas a tablazat harmadik soranak masodik cellajahoz
+
+/**
+ * @type {string} //A harmadik sor harmadik ertekenek
+ */
+const rowThirdFogalmak = "kisregény" //Valtozo deklaralas a tablazat harmadik soranak harmadik cellajahoz
+
+
+//4. sor
+
+/**
+ * @type {string} //A negyedik sor elso ertekenek
+ */
+const rowFourthSzerzo = "Franz Kafka" //Valtozo deklaralas a tablazat negyedik soranak elso cellajahoz
+
+/**
+ * @type {string} //A negyedik sor masodik ertekenek
+ */
+
+const rowFourthMu = "A per" //Valtozo deklaralas a tablazat negyedik soranak masodik cellajahoz
+
+/**
+ * @type {string} //A negyedik sor harmadik ertekenek
+ */
+const rowFourthFogalmak = "regény" //Valtozo deklaralas a tablazat negyedik soranak harmadik cellajahoz
+
+
+//5. sor
+
+/**
+ * @type {string} //Az otodik sor elso ertekenek
+ */
+const rowFifthSzerzo = "Franz Kafka" //Valtozo deklaralas a tablazat otodik soranak elso cellajahoz
+
+/**
+ * @type {string} //Az otodik sor masodik ertekenek
+ */
+const rowFifthMu = "Az átváltozás" //Valtozo deklaralas a tablazat otodik soranak masodik cellajahoz
+
+/**
+ * @type {string} //Az otodik sor harmadik ertekenek
+ */
+const rowFifthFogalmak = "kisregény" //Valtozo deklaralas a tablazat otodik soranak harmadik cellajahoz
+
+
+console.log(headSzerzo + " | " + headMu + " | " + headFogalmak + " |") //A consolera kiírom a táblázat fejlécét
+console.log(rowFirstSzerzo + " | " + rowFirstMu + " | " + rowFirstFogalmak + " |") //kiírom a consolera a táblázat 1. sorát
+console.log(rowSecondSzerzo + " | " + rowSecondMu + " | " + rowSecondFogalmak + " |") //kiírom a consolera a táblázat 2. sorát
+console.log(rowThirdSzerzo + " | " + rowThirdMu + " | " + rowThirdFogalmak + " |") //kiírom a consolera a táblázat 3. sorát
+console.log(rowFourthSzerzo + " | " + rowFourthMu + " | " + rowFourthFogalmak + " |") //kiírom a consolera a táblázat 4. sorát
+console.log(rowFifthSzerzo + " | " + rowFifthMu + " | " + rowFifthFogalmak + " |") //kiírom a consolera a táblázat 5. sorát
