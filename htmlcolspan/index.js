3527d34a9a491f6a0650d6261d90cbfd1ae24ed9
Schmidt Benedek <schmidt.benedek_2022C@bolyia-szakkozep.hu>
2026-01-01 19:32:51 +0100

    col html simple esemeny reg

---
+++ htmlcolspan/index.js
@@ -0,0 +1,8 @@
+/**
+ * @type {HTMLButtonElement} a gomb ami a simpla sort intezi majd
+ */
+const simpleButton = document.getElementById("simplebutton") //lekerem a szimpla gombot
+
+simpleButton.addEventListener("click", function(){ //esenykezeleto keszitek a szimplagomb click esemenyere
+    console.log("Szimpla sor gombjanak kattintasa megtortent, mukodik") //console loggal ellenorzom hogy mukodik e
+})
