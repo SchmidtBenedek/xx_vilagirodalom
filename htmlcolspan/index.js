bf66242de114a6dcd232640eaee413dd511c178c
Schmidt Benedek <schmidt.benedek_2022C@bolyia-szakkozep.hu>
2026-01-03 11:59:54 +0100

    col html esemeny fuggveny bev

--- htmlcolspan/index.js
+++ htmlcolspan/index.js
@@ -24,34 +24,7 @@ simpleButton.addEventListener("click", function(){ //esenykezeleto keszitek a sz
      */
     const tableBody = document.getElementById("tablebody") //elkerem a tablazat torzset
 
-    /**
-     * @type {HTMLTableRowElement} a tablazat uj sora
-     */
-    const row = document.createElement("tr") //sor letrehozasa
-    tableBody.appendChild(row) //hozzafuzom a tableBodyhoz
-
-    /**
-     * @type {HTMLTableCellElement} a tablazat tesztsoranak elso cellaja
-     */
-    const cell1 = document.createElement("td") //cella letrehozasa
-    row.appendChild(cell1) //hozzaadom az uj sorhoz
-    cell1.innerText = newRow.writer //beallitom a writer tulajdonsagat a newRow objektumbol
-
-    /**
-     * @type {HTMLTableCellElement} a tablazat tesztsoranak masodik cellaja
-     */
-    const cell2 = document.createElement("td") //cella letrehozasa
-    row.appendChild(cell2) //hozzaadom az uj sorhoz
-    cell2.innerText = newRow.work //beallitom a work tulajdonsagat a newRow objektumbol
-
-    /**
-     * @type {HTMLTableCellElement} a tablazat tesztsoranak harmadik cellaja
-     */
-    const cell3 = document.createElement("td") //cella letrehozasa
-    row.appendChild(cell3) //hozzaadom az uj sorhoz
-    cell3.innerText = newRow.concept1 //beallitom a concept1 tulajdonsagat a newRow objektumbol
-    cell3.colSpan = 2 //cell3 colSnap erteket 2 re allitom
-
+    addRow(newRow, tableBody) //meghivom a fuggvenyt es hozzafuzi
 })
 
 
@@ -78,6 +51,17 @@ doubleButton.addEventListener("click", function(){ //esenykezeleto keszitek a du
      */
     const tableBody = document.getElementById("tablebody") //elkerem a tablazat torzset
 
+    addRow(newRow, tableBody) //meghivom a fuggvenyt es hozzafuzi
+})
+
+/**
+ * Egy uj sort ad hozza a tablazathoz
+ * 
+ * @param {LiteratureDatas} newRow amit hozzafuzunk a tablazathoz
+ * @param {HTMLTableSectionElement} tableBody a tablazat tozs ahova hozzafuzom az uj sort 
+ * @returns {void}
+ */
+function addRow(newRow, tableBody){ //fugvenyt definialok 2 parameterreé
     /**
      * @type {HTMLTableRowElement} a tablazat uj sora
      */
@@ -105,13 +89,15 @@ doubleButton.addEventListener("click", function(){ //esenykezeleto keszitek a du
     row.appendChild(cell3) //hozzaadom az uj sorhoz
     cell3.innerText = newRow.concept1 //beallitom a concept1 tulajdonsagat a newRow objektumbol
 
-    /**
-     * @type {HTMLTableCellElement} //a tablazat tesztsoranak negyedik cellaja
-     */
-    const cell4 = document.createElement("td") //cella letrehozasa
-    row.appendChild(cell4) //hozzaadom az uj sorhoz 
-    cell4.innerText = newRow.concept2 //beallitom a concept2 tulajdonsagat a newRow objektumbol
-
-
-})
-
+    if(newRow.concept2){ //vizsgalom hogy definialva van e a concept2
+        /**
+         * @type {HTMLTableCellElement} //a tablazat tesztsoranak negyedik cellaja
+         */
+        const cell4 = document.createElement("td") //cella letrehozasa
+        row.appendChild(cell4) //hozzaadom az uj sorhoz 
+        cell4.innerText = newRow.concept2 //beallitom a concept2 tulajdonsagat a newRow objektumbol
+    }
+    else{ //egyebkent
+        cell3.colSpan = 2 //cell3 colSpan erteket 2 re allitomn
+    }
+}
